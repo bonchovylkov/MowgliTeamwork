1c62912cf571ce7a6bafe2ca9fcabc7495643bd5
Simeon Nikolov <simeon.v.nikolov@gmail.com>
2013-08-14 17:16:48 +0300

    fixed recipes controller

--- RecipeApi/Controllers/RecipesController.cs
+++ RecipeApi/Controllers/RecipesController.cs
@@ -6,46 +6,71 @@ using System.Net.Http;
 using System.Web.Http;
 using RecipeRepository;
 using RecipeApi.Models;
+using RecipeData;
+using RecipeModels;
 
 namespace RecipeApi.Controllers
 {
     public class RecipesController : ApiController
     {
+        private readonly IRepository<Recipe> recipeRepository;
+
         public RecipesController()
         {
-
+            this.recipeRepository = new DbRepositoryEF<Recipe>(new RecipeContext());
         }
 
-        public RecipesController(IRepository<RecepiesModel> repository)
+        public RecipesController(IRepository<Recipe> repository)
         {
-
+            this.recipeRepository = repository;
         }
 
         // GET api/recipes
-        public IEnumerable<string> Get()
+        public IEnumerable<RecepiesModel> Get()
         {
-            return new string[] { "value1", "value2" };
+            var allRecipes = this.recipeRepository.All();
+            var allRecipesModel = ConvertRecipesToRecipesModel(allRecipes);
+            return allRecipesModel.AsEnumerable();
         }
 
         // GET api/recipes/5
-        public string Get(int id)
+        public RecipiesModelFull Get(int id)
         {
-            return "value";
+            var recipe = this.recipeRepository.Get(id);
+            var recipeModel = ConverRecipeToRecipeModelFull(recipe);
+            return recipeModel;
         }
 
         // POST api/recipes
-        public void Post([FromBody]string value)
+        public void Post([FromBody]Recipe value)
         {
+
         }
 
         // PUT api/recipes/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]Recipe value)
         {
         }
 
         // DELETE api/recipes/5
         public void Delete(int id)
         {
+        }
+
+        private IEnumerable<RecepiesModel> ConvertRecipesToRecipesModel(IQueryable<Recipe> allRecipes)
+        {
+            throw new NotImplementedException();
+        }
+
+        private RecipiesModelFull ConverRecipeToRecipeModelFull(Recipe recipe)
+        {
+            var recipeModel = new RecepiesModel();
+            recipeModel.RecipeId = recipe.RecipeId;
+            recipeModel.RecipeName = recipe.RecipeName;
+            recipeModel.PictureLink = recipe.PictureLink;
+            recipeModel.Products = recipe.Products;
+            recipeModel.FromUser = recipe.User.UserName;
+            return null;
         }
     }
 }
