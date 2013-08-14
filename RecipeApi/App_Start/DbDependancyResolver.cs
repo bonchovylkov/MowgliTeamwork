2bf04838542d63f9403a50caeec95419f97af0b2
Simeon Nikolov <simeon.v.nikolov@gmail.com>
2013-08-14 16:35:19 +0300

    Added dependancy resolver and recipes controller

---
+++ RecipeApi/App_Start/DbDependancyResolver.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RecipeRepository;
+using RecipeApi.Models;
+using RecipeData;
+using System.Data.Entity;
+using RecipeApi.Controllers;
+
+namespace RecipeApi.App_Start
+{
+    public class DbDependencyResolver : IDependencyResolver
+    {
+        static IRepository<RecepiesModel> recipesRepository = 
+            new DbRepositoryEF<RecepiesModel>(new RecipeContext());
+        static IRepository<LikesModel> likesRepository =
+            new DbRepositoryEF<LikesModel>(new RecipeContext());
+        static IRepository<UserModel> usersRepository =
+            new DbRepositoryEF<UserModel>(new RecipeContext());
+        static IRepository<CommentsModel> commentsRepository =
+            new DbRepositoryEF<CommentsModel>(new RecipeContext());
+        static IRepository<StepsModel> stepsRepository =
+            new DbRepositoryEF<StepsModel>(new RecipeContext());
+
+        public object GetService(Type serviceType)
+        {
+            if (serviceType == typeof(RecipesController))
+            {
+                return new RecipesController(recipesRepository);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RecipeApi/Controllers/RecipesController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RecipeRepository;
+using RecipeApi.Models;
+
+namespace RecipeApi.Controllers
+{
+    public class RecipesController : ApiController
+    {
+        public RecipesController(IRepository<RecepiesModel> repository)
+        {
+
+        }
+
+        // GET api/recipes
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/recipes/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/recipes
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/recipes/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/recipes/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
